4f575c1a48b10a393f7f9f343588a548b028f7fb
hundessa <hundserb@gmail.com>
2024-12-14 09:17:44 -0800

    update on main and user controller file

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"Car_Rent_Backend/internal/helpers"
+	"Car_Rent_Backend/internal/models"
 	"Car_Rent_Backend/internal/routes"
 	"fmt"
 	"time"
@@ -9,6 +11,7 @@ import (
 	"github.com/gin-gonic/gin"
 	_ "github.com/joho/godotenv"
 	_ "github.com/lib/pq"
+	"gorm.io/gorm"
 
 	"Car_Rent_Backend/internal/migrations"
 )
@@ -23,6 +26,10 @@ func main() {
 	} else {
 		fmt.Println("Failed to connect to the database.")
 	}
+
+	// Check if an admin exists and create one if not
+	checkAndCreateAdmin(db)
+
 	// server := server.NewServer()
 	r := gin.Default()
 
@@ -44,4 +51,38 @@ func main() {
 		panic(fmt.Sprintf("cannot start server: %s", err))
 	}
 
+}
+
+// checkAndCreateAdmin checks if an admin exists; if not, it creates one
+func checkAndCreateAdmin(db *gorm.DB) {
+	var admin models.User
+	err := db.Where("role = ?", "admin").First(&admin).Error
+
+	// If no admin found, create a new one
+	if err == gorm.ErrRecordNotFound {
+		newAdmin := models.User{
+			FirstName: "admin",
+			Email:     "admin@mail.com",
+			Password:  "admin@ca", // You should hash the password here
+			Role:      "admin",
+		}
+
+		// Hash the password (replace with actual hashing)
+		hashedPassword, err := helpers.HashAndSalt([]byte(newAdmin.Password))
+		if err != nil {
+			fmt.Println("Error hashing password:", err)
+			return // Add this line to exit the function if there's an error hashing the password
+		}
+		newAdmin.Password = hashedPassword
+
+		if err := db.Create(&newAdmin).Error; err != nil {
+			fmt.Println("Error creating admin:", err)
+		} else {
+			fmt.Println("Admin user created successfully!")
+		}
+	} else if err != nil {
+		fmt.Println("Error checking for admin:", err)
+	} else {
+		fmt.Println("Admin already exists.")
+	}
 }
